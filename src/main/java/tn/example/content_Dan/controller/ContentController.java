4d63a48649f8fa064fdfb9a17194b6647d466f57
Marwen GHANNEM <marwen.ghannem@esprit.tn>
2023-05-27 13:23:34 +0100

    crud and query data

--- src/main/java/tn/example/content_Dan/controller/ContentController.java
+++ src/main/java/tn/example/content_Dan/controller/ContentController.java
@@ -1,17 +1,19 @@
 package tn.example.content_Dan.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import tn.example.content_Dan.model.Content;
+import tn.example.content_Dan.model.Status;
+import tn.example.content_Dan.model.Type;
 import tn.example.content_Dan.repository.ContentRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
-
     private final ContentRepository contentRepository;
 
     public ContentController(ContentRepository contentRepository) {
@@ -20,6 +22,43 @@ public class ContentController {
     @GetMapping
     public List<Content> getAllContent(){
         return contentRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Optional<Content> getContentById(@PathVariable Integer id){
+        return Optional.ofNullable(contentRepository.findById(id))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Content not found"));
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void addContent(@RequestBody Content content){
+        contentRepository.save(content);
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void updateContent(@RequestBody Content content,@PathVariable Integer id){
+        if (!contentRepository.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT,"Content not found");
+        }
+        contentRepository.save(content);
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteContentById(@PathVariable Integer id){
+        contentRepository.deleteById(id);
+    }
+    @GetMapping("/type/{type}")
+    public List<Content> contentByType(@PathVariable String type){
+        return contentRepository.findAllByContentType(type.toUpperCase());
+    }
+
+    @GetMapping("/status/{status}")
+    public List<Content> contentByStatus(@PathVariable Status status){
+        return contentRepository.listByStatus(status);
+    }
+
+    @PutMapping("query/{id}")
+    public void updateContentQuery(@PathVariable Integer id, @RequestBody Content content){
+        contentRepository.updateContent(content.title(), id);
     }
 
 }
--- src/main/java/tn/example/content_Dan/repository/ContentRepository.java
+++ src/main/java/tn/example/content_Dan/repository/ContentRepository.java
@@ -1,10 +1,26 @@
 package tn.example.content_Dan.repository;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 import tn.example.content_Dan.model.Content;
+import tn.example.content_Dan.model.Status;
 
-@Repository
+import java.util.List;
+
 public interface ContentRepository extends ListCrudRepository<Content, Integer> {
+    List<Content> findAllByContentType(String type);
+    @Query("""
+            SELECT * FROM content 
+            WHERE status = :status
+            """)
+    List<Content> listByStatus(@Param("status") Status status);
 
+    @Modifying
+    @Query("""
+            update content set title = :title 
+            where id = :id 
+            """)
+    void updateContent(@Param("title") String title, @Param("id") Integer id);
 }
